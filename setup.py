f50c7e08ecd9c18df796994ea7632e411d34d033
akshitjain16 <gang.akshitjain@gmail.com>
2025-03-12 13:55:23 +0530

    Add .gitignore, update README

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wasteDetection',
+    version='0.0.0',
+    description='Waste Detection',
+    author='Akshit Jain',
+    author_email='akshitjain5616@gmail.com',
+    packages=find_packages(),
+    license='MIT'
+)

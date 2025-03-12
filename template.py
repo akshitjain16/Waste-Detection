b617e00760562c52c67ee8d6072e9302a240190a
akshitjain16 <gang.akshitjain@gmail.com>
2025-03-12 12:59:37 +0530

    Initialize project structure

---
+++ template.py
@@ -0,0 +1,49 @@
+# to create a project template
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:') 
+
+project_name = "wasteDetection"
+
+list_of_files = [
+  ".github/workflows/.gitkeep",
+  "data/.gitkeep",
+  f"{project_name}/__init__.py",
+  f"{project_name}/components/__init__.py",  #this is a constructor for the project folder
+  f"{project_name}/components/data_ingestion.py",
+  f"{project_name}/components/data_validation.py",
+  f"{project_name}/components/model_trainer.py",
+  f"{project_name}/constants/__init__.py,"
+  f"{project_name}/constants/training_pipeline/__init__.py",
+  f"{project_name}/constants/application.py",
+  f"{project_name}/entity/config_entity.py",
+  f"{project_name}/entity/artifact_entity.py",
+  f"{project_name}/exception/__init__.py",
+  f"{project_name}/logger/__init__.py",
+  f"{project_name}/pipeline/__init__.py",
+  f"{project_name}/pipeline/training_pipeline.py",
+  f"{project_name}/utils/__init__.py",
+  f"{project_name}/utils/main_utils.py",
+  "templates/index.html",
+  "app.py",
+  "Dockerfile",
+  "requirements.txt",
+  "setup.py",
+] 
+
+for filepath in list_of_files: 
+  filepath = Path(filepath)  #this is to convert the string to path object
+  filedir, filename = os.path.split(filepath)  #this is to split the path into directory and file name)
+
+  if filedir != "":  #this is to check if the directory is not empty
+    os.makedirs(filedir, exist_ok=True)  #this is to create the directory if it does not exist
+    logging.info(f"Creating directory: {filedir} for the file: {filename}")
+
+  if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+      with open(filepath, "w") as f:
+        pass
+        logging.info(f"Creating empty file: {filepath}")
+  else:
+    logging.info(f"{filename} is already exists")
